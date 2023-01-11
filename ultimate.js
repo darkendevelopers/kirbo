fc5bfee93f4ce09987248c9666cc9883712bea8d
Darken248 <56902326+darken248@users.noreply.github.com>
2023-01-11 10:24:52 -0600

    e

--- ultimate.js
+++ ultimate.js
@@ -1,4 +1,3 @@
-var socket = io();
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'o';
 const WINNING_COMBINATIONS = [
@@ -11,223 +10,124 @@ const WINNING_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-const divtl = document.getElementById('tl');
-const divtm = document.getElementById('tm');
-const divtr = document.getElementById('tr');
-const divml = document.getElementById('ml');
-const divmm = document.getElementById('mm');
-const divmr = document.getElementById('mr');
-const divbl = document.getElementById('bl');
-const divbm = document.getElementById('bm');
-const divbr = document.getElementById('br');
-const tl = document.querySelectorAll('[data-celltl]');
-const tm = document.querySelectorAll('[data-celltm]');
-const tr = document.querySelectorAll('[data-celltr]');
-const ml = document.querySelectorAll('[data-cellml]');
-const mm = document.querySelectorAll('[data-cellmm]');
-const mr = document.querySelectorAll('[data-cellmr]');
-const bl = document.querySelectorAll('[data-cellbl]');
-const bm = document.querySelectorAll('[data-cellbm]');
-const br = document.querySelectorAll('[data-cellbr]');
-const section = document.querySelectorAll('[cell]');
-const board = document.getElementById('board');
+var circleTurn = false
 const winningMessageElement = document.getElementById('winningMessage');
-const winningMessageTextElement = document.querySelector(
-  '[data-winning-message-text]'
-);
-let circleTurn = false;
-setBoardHoverClass();
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+window.onload = restart()
 function restart() {
-  section.forEach((sec) => {
-    sec.className = 'first';
-    var a = document.querySelectorAll('[data-cell' + sec.id.toString() + ']');
-    a.forEach((b) => {
-      b.className = 'cell';
-    });
-  });
-  circleTurn = false;
-  setBoardHoverClass()
-  winningMessageElement.classList.remove('show')
-}
-function action(s, c) {
-  var id = document.getElementById(s + c);
-  var sec = document.getElementById(s);
-  var currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-  if (id.className != 'cell') return;
-  if (sec.className != 'first') return;
-  id.classList.add(currentClass);
-  checksec(currentClass);
-  checktie();
-  if (checkwin(currentClass)) {
-    section.forEach((cell) => {
-      cell.classList.add('wait');
-    });
-    winningMessageTextElement.innerText = `${currentClass.toUpperCase()}'s Wins!`;
-    winningMessageElement.classList.add('show');
-  } else if (isDraw()) {
-    section.forEach((cell) => {
-      cell.classList.add('wait');
-    });
-    winningMessageTextElement.innerText = 'Draw!';
-    winningMessageElement.classList.add('show');
-  } else {
-    circleTurn = !circleTurn;
+    board();
+    circleTurn = false;
     setBoardHoverClass();
-  }
+}
+function board() {
+    winningMessageTextElement.innerText = '';
+    winningMessageElement.classList.remove('show');
+    document.getElementById('board').innerHTML = ''
+    for(i = 1; i < 10; i++) {
+        if(i===1){var s='tl'}else if(i===2){var s='tm'}else if(i===3){var s='tr'}else if(i===4){var s='ml'}else if(i===5){var s='mm'}else if(i===6){var s='mr'}else if(i===7){var s='bl'}else if(i===8){var s='bm'}else{var s='br'}
+        var section = document.createElement('div')
+        section.id = s
+        section.className = 'first'
+        section.setAttribute('data', '')
+        document.getElementById('board').appendChild(section)
+        for(o = 1; o < 10; o++) {
+            if(o===1){var se='tl'}else if(o===2){var se='tm'}else if(o===3){var se='tr'}else if(o===4){var se='ml'}else if(o===5){var se='mm'}else if(o===6){var se='mr'}else if(o===7){var se='bl'}else if(o===8){var se='bm'}else{var se='br'}
+            var sect = document.createElement('div')
+            sect.id = s + se
+            sect.className = 'cell'
+            sect.setAttribute('data' + s, '')
+            sect.setAttribute("onClick", `action('${s}', '${se}');`);
+            section.appendChild(sect)
+        }
+    }
+}
+function action(s, ss, sss) {
+    var section = document.querySelectorAll('[data]')
+    var sec = document.getElementById(s + ss);
+    var sections = document.getElementById(s);
+    var currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    if (sec.className != 'cell' || section.className != 'first') return;
+    sex.classList.add(currentClass);
+    checksection(currentClass);
+    checkcelltie()
+    if (checkwin(currentClass)) {
+        section.forEach((cell) => {
+            cell.classList.add('wait');
+        });
+        winningMessageTextElement.innerText = `${currentClass.toUpperCase()}'s Wins!`;
+        winningMessageElement.classList.add('show');
+    } else if (isDraw() === true) {
+        section.forEach((cell) => {
+            cell.classList.add('wait');
+        });
+        winningMessageTextElement.innerText = 'Draw!';
+        winningMessageElement.classList.add('show');
+    } else {
+        circleTurn = !circleTurn;
+        setBoardHoverClass();
+    }
 }
 function setBoardHoverClass() {
-  board.classList.remove(X_CLASS);
-  board.classList.remove(CIRCLE_CLASS);
-  if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS);
-  } else {
-    board.classList.add(X_CLASS);
-  }
-}
-function isDraw() {
-  return [...section].every((cell) => {
-    return (
-      cell.classList.contains(X_CLASS) ||
-      cell.classList.contains(CIRCLE_CLASS) ||
-      cell.classList.contains('t')
-    );
-  });
-}
-function checkwin(currentClass) {
-  return WINNING_COMBINATIONS.some((combination) => {
-    return combination.every((index) => {
-      return section[index].classList.contains(currentClass);
-    });
-  });
-}
-function checktie() {
-  section.forEach((sec) => {
-    var a = document.querySelectorAll('[data-cell' + sec.id.toString() + ']');
-    var b = [...a].every((cell) => {
-      return (
-        cell.className.includes(X_CLASS) ||
-        cell.className.includes(CIRCLE_CLASS)
-      );
-    });
-    if (b) document.getElementById(sec.id).classList.add('t');
-  });
+    var board = document.getElementById('board')
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if (circleTurn) {
+      board.classList.add(CIRCLE_CLASS);
+    } else {
+      board.classList.add(X_CLASS);
+    }
 }
 function checksec(currentClass) {
-  if (divtl.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return tl[index].classList.contains(currentClass);
-      });
-      if (a === true) {
-        tl.forEach((c) => {
-          c.className = 'cell';
+    document.querySelectorAll('[data]').forEach((sec) => {
+        if (sec.className === 'first') {
+            var data = document.querySelectorAll('[data' + sec.id.toString() + ']');
+            data.forEach((cell) => {
+            if (cell.className === 'cell') {
+                WINNING_COMBINATIONS.some((combination) => {
+                    var d = document.querySelectorAll('[data' + cell.id.toString() + ']');
+                    var a = combination.every((index) => {
+                        return d[index].classList.contains(currentClass);
+                    });
+                    if (a === true) {
+                        cell.classList.add(currentClass)
+                        d.forEach(c => {
+                            c.className = 'cell'
+                        })
+                    }
+                })
+            }
         });
-        divtl.classList.add(currentClass);
       }
     });
-  }
-  if (divtm.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return tm[index].classList.contains(currentClass);
-      });
-      if (a === true) {
-        tm.forEach((c) => {
-          c.className = 'cell';
-        });
-        divtm.classList.add(currentClass);
-      }
+}
+function checkcelltie() {
+    var section = document.querySelectorAll('[data]')
+    section.forEach((sec) => {
+        if (sec.className === 'first') {
+            var data = document.querySelectorAll('[data' + sec.id.toString() + ']');
+            data.forEach((a) => {
+                if (a.className === 'cell') {
+                    var s = document.querySelectorAll('[data' + a.id.toString() + ']');
+                    var b = [...s].every((cell) => {
+                        return (cell.className.includes(X_CLASS) || cell.className.includes(CIRCLE_CLASS));
+                    });
+                    if (b) a.classList.add('t')
+                }
+            });
+        }
+    })
+}
+function isDraw() {
+    var a = document.querySelectorAll('[data]')
+    return [...a].every((cell) => {
+        return (cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS) || cell.classList.contains('t'));
     });
-  }
-  if (divtr.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return tr[index].classList.contains(currentClass);
+}
+function checkwin(currentClass) {
+    return WINNING_COMBINATIONS.some((combination) => {
+        return combination.every((index) => {
+            var a = document.querySelectorAll('[data]')
+            return a[index].classList.contains(currentClass);
       });
-      if (a === true) {
-        tr.forEach((c) => {
-          c.className = 'cell';
-        });
-        divtr.classList.add(currentClass);
-      }
     });
-  }
-  if (divml.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return ml[index].classList.contains(currentClass);
-      });
-      if (a === true) {
-        ml.forEach((c) => {
-          c.className = 'cell';
-        });
-        divml.classList.add(currentClass);
-      }
-    });
-  }
-  if (divmm.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return mm[index].classList.contains(currentClass);
-      });
-      if (a === true) {
-        mm.forEach((c) => {
-          c.className = 'cell';
-        });
-        divmm.classList.add(currentClass);
-      }
-    });
-  }
-  if (divmr.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return mr[index].classList.contains(currentClass);
-      });
-      if (a === true) {
-        mr.forEach((c) => {
-          c.className = 'cell';
-        });
-        divmr.classList.add(currentClass);
-      }
-    });
-  }
-  if (divbl.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return bl[index].classList.contains(currentClass);
-      });
-      if (a === true) {
-        bl.forEach((c) => {
-          c.className = 'cell';
-        });
-        divbl.classList.add(currentClass);
-      }
-    });
-  }
-  if (divbm.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return bm[index].classList.contains(currentClass);
-      });
-      if (a === true) {
-        bm.forEach((c) => {
-          c.className = 'cell';
-        });
-        divbm.classList.add(currentClass);
-      }
-    });
-  }
-  if (divbr.className === 'first') {
-    WINNING_COMBINATIONS.some((combination) => {
-      var a = combination.every((index) => {
-        return br[index].classList.contains(currentClass);
-      });
-      if (a === true) {
-        br.forEach((c) => {
-          c.className = 'cell';
-        });
-        divbr.classList.add(currentClass);
-      }
-    });
-  }
 }
